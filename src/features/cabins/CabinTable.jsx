ed40e971d7228611fe5e84c255b77f40cfa6d544
lhk <dklin1995@gmail.com>
2026-03-19 10:06:19 +0700

    sorted function in Cabins

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -14,21 +14,27 @@ const CabinTable = () => {
     return <Spinner />;
   }
 
+  // 1) FILTER
   const filterValue = searchParams.get("discount") || "all";
 
   let filteredCabins;
-
   if (filterValue === "all") {
     filteredCabins = cabins;
   }
-
   if (filterValue === "no-discount") {
     filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
   }
-
   if (filterValue === "with-discount") {
     filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
   }
+
+  // 2) SORT
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier,
+  );
 
   return (
     <Menus>
@@ -43,7 +49,7 @@ const CabinTable = () => {
         </Table.Header>
 
         <Table.Body
-          data={filteredCabins}
+          data={sortedCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
