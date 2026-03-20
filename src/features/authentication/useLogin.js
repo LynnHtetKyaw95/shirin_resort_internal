f562a279ddbfff53333977d86dbd24b9a9ac5d9f
lhk <dklin1995@gmail.com>
2026-03-20 17:15:56 +0700

    authentication: user login with supabase

---
+++ src/features/authentication/useLogin.js
@@ -0,0 +1,21 @@
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router";
+import { login as loginAPI } from "../../services/apiAuth";
+import toast from "react-hot-toast";
+
+export function useLogin() {
+  const navigate = useNavigate();
+
+  const { mutate: login, isPending } = useMutation({
+    mutationFn: ({ email, password }) => loginAPI({ email, password }),
+    onSuccess: (user) => {
+      navigate("/dashboard");
+    },
+    onError: (error) => {
+      console.log("ERROR", error);
+      toast.error("Provided email or password are incorrect");
+    },
+  });
+
+  return { login, isPending };
+}
